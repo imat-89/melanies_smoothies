e09a36f05d3705d8107866a948349a2251a88a71
imat-89 <imatijevic89@gmail.com>
2025-05-13 14:42:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -49,6 +50,3 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()  
         st.success('Your Smoothie is ordered, '+name_on_order, icon="✅")
-
-import requests
-
